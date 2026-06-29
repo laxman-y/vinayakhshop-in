d1b7dd4c9750622eeb888c0acdcb57e327eff0e6
laxman-y <laxmanyadav.dev@gmail.com>
2026-06-29 19:53:28 +0530

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 import {
   Mail,
@@ -39,7 +40,7 @@ function Login() {
 
       const res = await axios.post(
 
-        "http://localhost:5000/api/auth/login",
+        `{BASE_URL}/api/auth/login`,
 
         {
 
